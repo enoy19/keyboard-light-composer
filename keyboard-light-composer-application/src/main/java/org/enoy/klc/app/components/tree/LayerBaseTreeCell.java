ec7e64e58578379b043d8a42cdb08b3f5f1ba463
enoy <enoy@enoySurface>
2017-04-30 13:00:18 +0200

    started implementing dnd functions to treecell

--- keyboard-light-composer-application/src/main/java/org/enoy/klc/app/components/tree/LayerBaseTreeCell.java
+++ keyboard-light-composer-application/src/main/java/org/enoy/klc/app/components/tree/LayerBaseTreeCell.java
@@ -5,11 +5,21 @@ import org.enoy.klc.common.layers.EffectLayer;
 import org.enoy.klc.common.layers.LayerBase;
 
 import javafx.scene.control.TreeCell;
+import javafx.scene.input.DragEvent;
 
-public class LayerBaseTreeCell extends TreeCell<LayerBaseContainer<? extends LayerBase>> {
+public class LayerBaseTreeCell
+		extends
+			TreeCell<LayerBaseContainer<? extends LayerBase>> {
+
+	public LayerBaseTreeCell() {
+
+		setOnDragOver(this::onDragOver);
+
+	}
 
 	@Override
-	protected void updateItem(LayerBaseContainer<? extends LayerBase> item, boolean empty) {
+	protected void updateItem(LayerBaseContainer<? extends LayerBase> item,
+			boolean empty) {
 
 		if (!empty && item != null) {
 			LayerBase layerBase = item.getLayerBase();
@@ -18,18 +28,24 @@ public class LayerBaseTreeCell extends TreeCell<LayerBaseContainer<? extends Lay
 
 			if (layerBase instanceof EffectLayer) {
 				EffectLayer effectLayer = (EffectLayer) layerBase;
-				name = effectLayer.getEffectLayerInformation().getName().getValue();
+				name = effectLayer.getEffectLayerInformation().getName()
+						.getValue();
 			} else if (layerBase instanceof EffectGroupLayer) {
 				EffectGroupLayer effectGroupLayer = (EffectGroupLayer) layerBase;
-				name = effectGroupLayer.getEffectLayerInformation().getName().getValue();
+				name = effectGroupLayer.getEffectLayerInformation().getName()
+						.getValue();
 			}
-			
+
 			setText(name);
 
 		} else {
 			setText(null);
 		}
 
+	}
+	
+	private void onDragOver(DragEvent event){
+		
 	}
 
 }
